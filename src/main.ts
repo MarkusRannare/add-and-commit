216d61e3268d093370f1886c8408178393a3ee39
Markus Rännare <mackyman@gmail.com>
2021-01-31 22:37:18 +0100

    Logging all branches

--- src/main.ts
+++ src/main.ts
@@ -52,6 +52,9 @@ console.log(`Running in ${baseDir}`)
       getInput('branch')
     ])
 
+    const allBranches = await git.raw('branch', '-a')
+    info("All branches is \n" + allBranches)
+
     // listRemoteResults is true if 'branch' exists
     const desiredBranch = listRemoteResults
       ? getInput('branch')
